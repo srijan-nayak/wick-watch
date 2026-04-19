4f8428641a175d238bec7ad0fa603a5624867b2a
Srijan Nayak <srijannayak0@gmail.com>
2026-04-19 20:17:42 +0530

    Load .env on startup

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 import os
 import logging
 from contextlib import asynccontextmanager
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
